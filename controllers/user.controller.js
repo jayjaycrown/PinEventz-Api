bdef3ac0db013be9d3a835719c6985bad11ab779
jayjaycrown <jayjaycrown@gmail.com>
2019-12-02 08:53:08 +0100

    Updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const passport = require('passport');
 const _ = require('lodash');
+const jwt = require('jasonwebtoken');
 
 const User = mongoose.model('User');
 
@@ -29,7 +30,7 @@ module.exports.register = (req, res, next) => {
             var token;
             token = user.generateJwt();
             res.json({
-                "token" : token
+                token : token
             });
         }
 
@@ -49,7 +50,21 @@ module.exports.authenticate = (req, res, next) => {
         // error from passport middleware
         if (err) return res.status(400).json(err);
         // registered user
-        else if (user) return res.status(200).json({ "token": user.generateJwt() });
+        else if (user) {
+          const token = jwt.sign({
+            email: user.email,
+            userId: user._id
+          },
+           process.env.JWT_SECRET,
+           {
+              expiresIn: process.env.JWT_EXP
+           });
+          return res.status(200).json({
+            message: "Authentication Successful",
+            token: token,
+            "token": user.generateJwt()
+          })
+      }
         // unknown user or wrong password
         else return res.status(404).json(info);
     })(req, res);
@@ -60,8 +75,7 @@ module.exports.userProfile = (req, res, next) =>{
         (err, user) => {
             if (!user)
                 return res.status(404).json({ status: false, message: 'User record not found.' });
-            else
-                return res.status(200).json({ status: true, user : _.pick(user,['fullName','email']) });
+            else return res.status(200).json({ status: true, user : _.pick(user,['fullName','email'])});
         }
     );
 }
